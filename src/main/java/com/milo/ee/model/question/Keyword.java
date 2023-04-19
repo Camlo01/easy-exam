19666ba01025bed2b393dcaa99d40ef1cbea799a
unknown <camilobeltran174@gmail.com>
2023-04-18 19:50:39 -0500

    CORRECT variable names | ADD constructors | REMOVE unused getters and setters

--- src/main/java/com/milo/ee/model/question/Keyword.java
+++ src/main/java/com/milo/ee/model/question/Keyword.java
@@ -2,97 +2,105 @@ package com.milo.ee.model.question;
 
 public class Keyword extends Question {
 
-    private String AnswerOption1;
-    private String AnswerOption2;
-    private String AnswerOption3;
-    private String AnswerOption4;
-    private String AnswerOption5;
-    private String AnswerOption6;
-    private String AnswerOption7;
-    private String AnswerOption8;
-    private String AnswerOption9;
-    private String AnswerOption10;
+    private String answer1;
+    private String answer2;
+    private String answer3;
+    private String answer4;
+    private String answer5;
+    private String answer6;
+    private String answer7;
+    private String answer8;
+    private String answer9;
+    private String answer10;
 
-    public Keyword() {
+    public Keyword(String questionText, String questionType, String answer1) {
+        super(questionText, questionType);
+        this.answer1 = answer1;
     }
 
-    public String getAnswerOption1() {
-        return AnswerOption1;
+    public Keyword(String questionText, String questionType, String answer1, String answer2) {
+        super(questionText, questionType);
+        this.answer1 = answer1;
+        this.answer2 = answer2;
     }
 
-    public void setAnswerOption1(String answerOption1) {
-        AnswerOption1 = answerOption1;
+    public String getAnswer1() {
+        return answer1;
     }
 
-    public String getAnswerOption2() {
-        return AnswerOption2;
+    public void setAnswer1(String answer1) {
+        this.answer1 = answer1;
     }
 
-    public void setAnswerOption2(String answerOption2) {
-        AnswerOption2 = answerOption2;
+    public String getAnswer2() {
+        return answer2;
     }
 
-    public String getAnswerOption3() {
-        return AnswerOption3;
+    public void setAnswer2(String answer2) {
+        this.answer2 = answer2;
     }
 
-    public void setAnswerOption3(String answerOption3) {
-        AnswerOption3 = answerOption3;
+    public String getAnswer3() {
+        return answer3;
     }
 
-    public String getAnswerOption4() {
-        return AnswerOption4;
+    public void setAnswer3(String answer3) {
+        this.answer3 = answer3;
     }
 
-    public void setAnswerOption4(String answerOption4) {
-        AnswerOption4 = answerOption4;
+    public String getAnswer4() {
+        return answer4;
     }
 
-    public String getAnswerOption5() {
-        return AnswerOption5;
+    public void setAnswer4(String answer4) {
+        this.answer4 = answer4;
     }
 
-    public void setAnswerOption5(String answerOption5) {
-        AnswerOption5 = answerOption5;
+    public String getAnswer5() {
+        return answer5;
     }
 
-    public String getAnswerOption6() {
-        return AnswerOption6;
+    public void setAnswer5(String answer5) {
+        this.answer5 = answer5;
     }
 
-    public void setAnswerOption6(String answerOption6) {
-        AnswerOption6 = answerOption6;
+    public String getAnswer6() {
+        return answer6;
     }
 
-    public String getAnswerOption7() {
-        return AnswerOption7;
+    public void setAnswer6(String answer6) {
+        this.answer6 = answer6;
     }
 
-    public void setAnswerOption7(String answerOption7) {
-        AnswerOption7 = answerOption7;
+    public String getAnswer7() {
+        return answer7;
     }
 
-    public String getAnswerOption8() {
-        return AnswerOption8;
+    public void setAnswer7(String answer7) {
+        this.answer7 = answer7;
     }
 
-    public void setAnswerOption8(String answerOption8) {
-        AnswerOption8 = answerOption8;
+    public String getAnswer8() {
+        return answer8;
     }
 
-    public String getAnswerOption9() {
-        return AnswerOption9;
+    public void setAnswer8(String answer8) {
+        this.answer8 = answer8;
     }
 
-    public void setAnswerOption9(String answerOption9) {
-        AnswerOption9 = answerOption9;
+    public String getAnswer9() {
+        return answer9;
     }
 
-    public String getAnswerOption10() {
-        return AnswerOption10;
+    public void setAnswer9(String answer9) {
+        this.answer9 = answer9;
     }
 
-    public void setAnswerOption10(String answerOption10) {
-        AnswerOption10 = answerOption10;
+    public String getAnswer10() {
+        return answer10;
+    }
+
+    public void setAnswer10(String answer10) {
+        this.answer10 = answer10;
     }
 }
