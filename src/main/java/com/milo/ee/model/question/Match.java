56fa6aa149dc1c442f51cf33dceb38f826884572
unknown <camilobeltran174@gmail.com>
2023-04-18 19:52:07 -0500

    CORRECT variable names | REMOVE unused getters and setters

--- src/main/java/com/milo/ee/model/question/Match.java
+++ src/main/java/com/milo/ee/model/question/Match.java
@@ -2,187 +2,197 @@ package com.milo.ee.model.question;
 
 public class Match extends Question {
 
-    private String OptionText1;
-    private String AnswerOption1;
-    private String OptionText2;
-    private String AnswerOption2;
-    private String OptionText3;
-    private String AnswerOption3;
-    private String OptionText4;
-    private String AnswerOption4;
-    private String OptionText5;
-    private String AnswerOption5;
-    private String OptionText6;
-    private String AnswerOption6;
-    private String OptionText7;
-    private String AnswerOption7;
-    private String OptionText8;
-    private String AnswerOption8;
-    private String OptionText9;
-    private String AnswerOption9;
-    private String OptionText10;
-    private String AnswerOption10;
+    private String optionText1;
+    private String answerOption1;
+    private String optionText2;
+    private String answerOption2;
+    private String optionText3;
+    private String answerOption3;
+    private String optionText4;
+    private String answerOption4;
+    private String optionText5;
+    private String answerOption5;
+    private String optionText6;
+    private String answerOption6;
+    private String optionText7;
+    private String answerOption7;
+    private String optionText8;
+    private String answerOption8;
+    private String optionText9;
+    private String answerOption9;
+    private String optionText10;
+    private String answerOption10;
 
     public Match() {
     }
 
+    public Match(String questionText, String questionType, String optionText1, String answerOption1, String optionText2, String answerOption2, String optionText3, String answerOption3) {
+        super(questionText, questionType);
+        this.optionText1 = optionText1;
+        this.answerOption1 = answerOption1;
+        this.optionText2 = optionText2;
+        this.answerOption2 = answerOption2;
+        this.optionText3 = optionText3;
+        this.answerOption3 = answerOption3;
+    }
+
     public String getOptionText1() {
-        return OptionText1;
+        return optionText1;
     }
 
     public void setOptionText1(String optionText1) {
-        OptionText1 = optionText1;
+        this.optionText1 = optionText1;
     }
 
     public String getAnswerOption1() {
-        return AnswerOption1;
+        return answerOption1;
     }
 
     public void setAnswerOption1(String answerOption1) {
-        AnswerOption1 = answerOption1;
+        this.answerOption1 = answerOption1;
     }
 
     public String getOptionText2() {
-        return OptionText2;
+        return optionText2;
     }
 
     public void setOptionText2(String optionText2) {
-        OptionText2 = optionText2;
+        this.optionText2 = optionText2;
     }
 
     public String getAnswerOption2() {
-        return AnswerOption2;
+        return answerOption2;
     }
 
     public void setAnswerOption2(String answerOption2) {
-        AnswerOption2 = answerOption2;
+        this.answerOption2 = answerOption2;
     }
 
     public String getOptionText3() {
-        return OptionText3;
+        return optionText3;
     }
 
     public void setOptionText3(String optionText3) {
-        OptionText3 = optionText3;
+        this.optionText3 = optionText3;
     }
 
     public String getAnswerOption3() {
-        return AnswerOption3;
+        return answerOption3;
     }
 
     public void setAnswerOption3(String answerOption3) {
-        AnswerOption3 = answerOption3;
+        this.answerOption3 = answerOption3;
     }
 
     public String getOptionText4() {
-        return OptionText4;
+        return optionText4;
     }
 
     public void setOptionText4(String optionText4) {
-        OptionText4 = optionText4;
+        this.optionText4 = optionText4;
     }
 
     public String getAnswerOption4() {
-        return AnswerOption4;
+        return answerOption4;
     }
 
     public void setAnswerOption4(String answerOption4) {
-        AnswerOption4 = answerOption4;
+        this.answerOption4 = answerOption4;
     }
 
     public String getOptionText5() {
-        return OptionText5;
+        return optionText5;
     }
 
     public void setOptionText5(String optionText5) {
-        OptionText5 = optionText5;
+        this.optionText5 = optionText5;
     }
 
     public String getAnswerOption5() {
-        return AnswerOption5;
+        return answerOption5;
     }
 
     public void setAnswerOption5(String answerOption5) {
-        AnswerOption5 = answerOption5;
+        this.answerOption5 = answerOption5;
     }
 
     public String getOptionText6() {
-        return OptionText6;
+        return optionText6;
     }
 
     public void setOptionText6(String optionText6) {
-        OptionText6 = optionText6;
+        this.optionText6 = optionText6;
     }
 
     public String getAnswerOption6() {
-        return AnswerOption6;
+        return answerOption6;
     }
 
     public void setAnswerOption6(String answerOption6) {
-        AnswerOption6 = answerOption6;
+        this.answerOption6 = answerOption6;
     }
 
     public String getOptionText7() {
-        return OptionText7;
+        return optionText7;
     }
 
     public void setOptionText7(String optionText7) {
-        OptionText7 = optionText7;
+        this.optionText7 = optionText7;
     }
 
     public String getAnswerOption7() {
-        return AnswerOption7;
+        return answerOption7;
     }
 
     public void setAnswerOption7(String answerOption7) {
-        AnswerOption7 = answerOption7;
+        this.answerOption7 = answerOption7;
     }
 
     public String getOptionText8() {
-        return OptionText8;
+        return optionText8;
     }
 
     public void setOptionText8(String optionText8) {
-        OptionText8 = optionText8;
+        this.optionText8 = optionText8;
     }
 
     public String getAnswerOption8() {
-        return AnswerOption8;
+        return answerOption8;
     }
 
     public void setAnswerOption8(String answerOption8) {
-        AnswerOption8 = answerOption8;
+        this.answerOption8 = answerOption8;
     }
 
     public String getOptionText9() {
-        return OptionText9;
+        return optionText9;
     }
 
     public void setOptionText9(String optionText9) {
-        OptionText9 = optionText9;
+        this.optionText9 = optionText9;
     }
 
     public String getAnswerOption9() {
-        return AnswerOption9;
+        return answerOption9;
     }
 
     public void setAnswerOption9(String answerOption9) {
-        AnswerOption9 = answerOption9;
+        this.answerOption9 = answerOption9;
     }
 
     public String getOptionText10() {
-        return OptionText10;
+        return optionText10;
     }
 
     public void setOptionText10(String optionText10) {
-        OptionText10 = optionText10;
+        this.optionText10 = optionText10;
     }
 
     public String getAnswerOption10() {
-        return AnswerOption10;
+        return answerOption10;
     }
 
     public void setAnswerOption10(String answerOption10) {
-        AnswerOption10 = answerOption10;
+        this.answerOption10 = answerOption10;
     }
 }
