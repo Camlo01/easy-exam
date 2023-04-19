a507527432a4505e450564c9ec09e78db24192af
unknown <camilobeltran174@gmail.com>
2023-04-18 21:14:26 -0500

    ADDED Jackson annotation

--- src/main/java/com/milo/ee/model/question/Freetext.java
+++ src/main/java/com/milo/ee/model/question/Freetext.java
@@ -1,14 +1,17 @@
 package com.milo.ee.model.question;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
 /**
  * Abstract class Question that include field that every question has
  *
  * @author Milo
  */
+@JsonTypeName("FREETEXT")
 public class Freetext extends Question {
 
-    public Freetext(String questionText, String questionType) {
-        super(questionText, questionType);
+    public Freetext(String type, String questionText, String questionType) {
+        super(type, questionText, questionType);
     }
 
     @Override
